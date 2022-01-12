921ee1bb580fd6029aa44fe74b59a6c4e5f679b0
Guru Narayanan <guruknarayanan@gmail.com>
2022-01-12 17:50:48 -0500

    Initial commit with cassandra docker compose file

---
+++ myretail-rest/src/main/java/com/cs/myretail/myretailrest/MyretailRestApplication.java
@@ -0,0 +1,13 @@
+package com.cs.myretail.myretailrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyretailRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyretailRestApplication.class, args);
+	}
+
+}
---
+++ myretail-rest/src/test/java/com/cs/myretail/myretailrest/MyretailRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.myretail.myretailrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyretailRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
